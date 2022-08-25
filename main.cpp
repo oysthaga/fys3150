86c77e67c2c3197d1a9a42940975aa1e636ff6f9
oysthaga <112075223+oysthaga@users.noreply.github.com>
2022-08-25 14:41:38 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+// Look, our first C++ program!
+
+#include <iostream>
+
+int main()
+{
+  // Print a message to screen
+  std::cout << "Fuck the World!\n";
+
+  // Return 0 when done
+  return 0;
+}
